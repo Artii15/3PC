6a093360cdf9c1846cd41a737ca2f9764513c5f8
Artii15 <artur.kowalski15@gmail.com>
2016-12-27 10:35:56 +0100

    Messages log

--- src/main/scala/tpc/demo/operations/AppendLogOperation.scala
+++ src/main/scala/tpc/demo/operations/AppendLogOperation.scala
@@ -2,6 +2,8 @@ package tpc.demo.operations
 
 import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.file.{Files, Paths, StandardOpenOption}
+import java.text.SimpleDateFormat
+import java.util.Date
 
 import tpc.transactions.Operation
 
@@ -11,13 +13,16 @@ class AppendLogOperation(content: String) extends Operation {
 
   override def execute(): Unit = {
     val fileWriter = new BufferedWriter(new FileWriter(temporaryFile))
-    fileWriter.write(content)
+    val formattedDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
+    fileWriter.write(s"$formattedDate\n")
+    fileWriter.write(s"$content\n\n")
+    fileWriter.close()
   }
 
   override def rollback(): Unit = cleanUp()
 
   override def commit(): Unit = {
-    Files.write(Paths.get("~/log.txt"), Files.readAllBytes(temporaryFilePath), StandardOpenOption.APPEND)
+    Files.write(Paths.get("/home/artur/log.txt"), Files.readAllBytes(temporaryFilePath), StandardOpenOption.APPEND, StandardOpenOption.CREATE)
     cleanUp()
   }
 
