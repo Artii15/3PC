bbeb30f087bc00e516c982cdfc3069c26240afcf
Artii15 <artur.kowalski15@gmail.com>
2017-01-23 23:54:01 +0100

    Temporary files dropped

--- src/main/scala/tpc/demo/operations/AppendLogOperation.scala
+++ src/main/scala/tpc/demo/operations/AppendLogOperation.scala
@@ -1,6 +1,5 @@
 package tpc.demo.operations
 
-import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.file.{Files, Paths, StandardOpenOption}
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -9,30 +8,22 @@ import com.typesafe.config.ConfigFactory
 import tpc.transactions.Operation
 
 class AppendLogOperation(content: String) extends Operation {
-  private var temporaryFile: Option[File] = None
+  private var unsavedContent = ""
 
   override def execute(): Unit = {
-    temporaryFile = Some(generateTempFile())
-    val fileWriter = new BufferedWriter(new FileWriter(temporaryFile.get))
     val formattedDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
-    fileWriter.write(s"$formattedDate\n")
-    fileWriter.write(s"$content\n\n")
-    fileWriter.close()
+    unsavedContent = s"$formattedDate\n$content\n\n"
   }
 
   override def rollback(): Unit = cleanUp()
 
   override def commit(executorId: Int): Unit = {
-    temporaryFile.foreach(tempFile => {
-      Files.write(Paths.get(s"${AppendLogOperation.logFilePath}$executorId"),
-        Files.readAllBytes(Paths.get(tempFile.getAbsolutePath)), StandardOpenOption.APPEND, StandardOpenOption.CREATE)
-    })
+      Files.write(Paths.get(s"${AppendLogOperation.logFilePath}$executorId"), unsavedContent.getBytes,
+        StandardOpenOption.APPEND, StandardOpenOption.CREATE)
     cleanUp()
   }
 
-  private def cleanUp(): Unit = temporaryFile.map(_.delete())
-
-  private def generateTempFile() = File.createTempFile("tpc-log", ".tmp")
+  private def cleanUp() = unsavedContent = ""
 }
 
 object AppendLogOperation {
